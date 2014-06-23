b912466f3f1d7de912d36c2a3afe1ffd43917aaa
tung <ictvmt1@gmail.com>
2014-06-23 09:49:29 +0700

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -24,4 +25,9 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    def destroy
+    sign_out
+    redirect_to root_url
+  end
 end
