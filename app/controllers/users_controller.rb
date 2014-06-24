ca99b3f358c5a38009770b381a86054f249ae148
tung <ictvmt1@gmail.com>
2014-06-24 08:34:16 +0700

    Finish friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+before_action :signed_in_user, only: [:edit, :update]
+before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def new
@@ -28,6 +42,20 @@ class UsersController < ApplicationController
 
     def destroy
     sign_out
-    redirect_to root_url
+    redirect_to home_url
   end
+
+  # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(home_url) unless current_user?(@user)
+    end
 end
